8cf212fcb9fb7b98042bd31bda614a8109310988
valentinvale <valionsemen@gmail.com>
2022-05-30 21:26:57 +0300

    const ref

--- club_element.h
+++ club_element.h
@@ -13,26 +13,32 @@ class club_element {
 
 public:
     //club_element(T element) : element(element) {}
-    club_element() {}
+    club_element();
 
-    void setElement(T element_) {
-        club_element::element = element_;
-    }
+    void setElement(const T& element_);
 
-    friend std::ostream &operator<<(std::ostream &os, const club_element &element_) {
-        os << "element: " << element_.element;
-        return os;
-    }
+    T afisClubElement();
 
-    T afisClubElement(){
-        return element;
-    }
-
-    T getElement() const {
-        return element;
-    }
+    T getElement() const;
 
 };
 
+template<typename T>
+void club_element<T>::setElement(const T &element_) {
+    club_element::element = element_;
+}
+
+template<typename T>
+T club_element<T>::getElement() const {
+    return element;
+}
+
+template<typename T>
+T club_element<T>::afisClubElement() {
+    return element;
+}
+
+template<typename T>
+club_element<T>::club_element() {}
 
 #endif //OOP_CLUB_ELEMENT_H
